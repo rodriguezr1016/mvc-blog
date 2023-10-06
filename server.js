5e6c35e0e69d88bbe15de2c9d2691fc079ed49da
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-10-05 19:22:02 -0700

    I had to change the host back to localhost to deploy on heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 //setup session
 const sess = {
-  secret: 'supersecretsessionsecrettext',
+  secret: 'sessionsupersecret',
   cookie: { maxAge: 180000},
   resave: false,
   saveUninitialized: true,
